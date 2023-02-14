7a4fc01979b4c874182160a13ce8cb8142d82160
skimQ52 <skylarmawle@gmail.com>
2023-02-14 11:17:36 -0500

    quick cleanup

--- SkImage.py
+++ SkImage.py
@@ -63,20 +63,19 @@ class SkImage:
 
 
     def shear_at_point(self, angle, x, y):
-        '''
-        |1  -tan(𝜃/2) |  |1        0|  |1  -tan(𝜃/2) | 
-        |0      1     |  |sin(𝜃)   1|  |0      1     |
-        '''
+        # |1  -tan(𝜃/2) |  |1        0|  |1  -tan(𝜃/2) | 
+        # |0      1     |  |sin(𝜃)   1|  |0      1     |
+
         # shear 1
         tangent=math.tan(angle/2)
         new_x=round(x-y*tangent)
         new_y=y
         
-        #shear 2
-        new_y=round(new_x*math.sin(angle)+new_y)      #since there is no change in new_x according to the shear matrix
+        # shear 2
+        new_y=round(new_x*math.sin(angle)+new_y)
 
-        #shear 3
-        new_x=round(new_x-new_y*tangent)              #since there is no change in new_y according to the shear matrix
+        # shear 3
+        new_x=round(new_x-new_y*tangent)
         
         return new_y, new_x
 
